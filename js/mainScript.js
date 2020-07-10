083218d6c93ff94900903f06496e7e5937a3b864
HeoYongJe <hyj.campus@gmail.com>
2020-07-11 04:24:19 +0900

    -

--- js/mainScript.js
+++ js/mainScript.js
@@ -14,8 +14,9 @@ $(document).ready(function() {
 const $fadeTop = document.querySelector('.fadeTop');
 
 window.onload = function(){
-	
+
 }
+// 페이지 열릴 시 위에서 올라오는 스크립트 실행
 
 /*font-typing*/
 // $('#introPage h2').typeIt({
