ca3f36b8952d499c90be0344e11e88282e2257c0
HeoYongJe <hyj.campus@gmail.com>
2020-09-09 23:43:13 +0900

    클릭 시 컨텐츠 및 닫기버튼 활성화

--- js/mainScript.js
+++ js/mainScript.js
@@ -2,13 +2,23 @@
 // setbutton class
 const $fadeTop = document.querySelector('.fadeTop');
 const $introDivideLayout = document.querySelector('.introDivideLayout');
+const $figure = document.querySelector('.figure');
+const $figureContent = document.querySelector('.figureContent');
+const $closeBtn = document.querySelector('.closeBtn');
 const fadeTopOn = () => ([...$introDivideLayout.classList].includes('fadeTopOn'));
+
 
 // const $startWindow = function(){
 // 	fadeTopOn();
 // }
+const setClick = () =>{
+        $figureContent.style.display = 'block';
+        $figure.style.background ='rgba(0,0,0,0.7)';
+        $closeBtn.style.display = 'block';
+};
+$figure.addEventListener('click', setClick);
 
-$fadeTop.addEventListener('load', () => $fadeTopOn());
+// $fadeTop.addEventListener('load', () => $fadeTopOn());
 // 페이지 열릴 시 위에서 올라오는 스크립트 실행
 
 /*font-typing*/
