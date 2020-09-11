3e4cb0ce03794c98cf642b1a9ec9a0d4283049d2
HeoYongJe <hyj.campus@gmail.com>
2020-09-11 23:48:44 +0900

    수정

--- js/mainScript.js
+++ js/mainScript.js
@@ -2,21 +2,40 @@
 // setbutton class
 const $fadeTop = document.querySelector('.fadeTop');
 const $introDivideLayout = document.querySelector('.introDivideLayout');
-const $figure = document.querySelector('.figure');
+const $contentFloat = document.querySelector('.contentFloat');
 const $figureContent = document.querySelector('.figureContent');
 const $closeBtn = document.querySelector('.closeBtn');
+const $titleAndContent = document.querySelector('.titleAndContent');
 const fadeTopOn = () => ([...$introDivideLayout.classList].includes('fadeTopOn'));
 
 
 // const $startWindow = function(){
 // 	fadeTopOn();
 // }
-const setClick = () =>{
-        $figureContent.style.display = 'block';
-        $figure.style.background ='rgba(0,0,0,0.7)';
-        $closeBtn.style.display = 'block';
+const render = () =>{
+   for (let i = 0; i < contentFloat.length; i++) {
+        let item = contentFloat[i];
+        item.addEventListener('click', setClick);
+    } 
+
+    for (let i = 0; i < closeBtn.length; i++) {
+        let closeBtn = closeBtn[i];
+        closeBtn.addEventListener('click', closeClick);
+    }
+}
+
+const setClick = () => {
+        $titleAndContent.style.display = 'block';
+        $contentFloat.style.opacity ='0.5';
 };
-$figure.addEventListener('click', setClick);
+
+const closeClick = () => {
+    $titleAndContent.style.display = 'none';
+    $contentFloat.style.opacity ='1';
+
+};
+// $contentFloat.addEventListener('click', setClick);
+// $closeBtn.addEventListener('click', closeClick);
 
 // $fadeTop.addEventListener('load', () => $fadeTopOn());
 // 페이지 열릴 시 위에서 올라오는 스크립트 실행
