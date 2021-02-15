29c301936a70fb046ee11ff2ac107285b19e7b49
HeoYongJe <hyj.campus@gmail.com>
2021-02-16 00:43:32 +0900

    html 구조 및 css 변경작업

--- js/mainScript.js
+++ js/mainScript.js
@@ -1,31 +1,41 @@
 // setbutton class
 const $fadeTop = document.querySelector('.fadeTop');
 
-const contentFloat = document.querySelector('.contentFloat');
-const itemFloat = document.querySelector('.itemFloat');
-const figure = document.querySelector('.figure');
-const figureContent = document.querySelector('.figureContent');
-const hoverImg = document.querySelector('.hoverImg');
-const figcaption = document.getElementsByTagName('figcaption');
+const $contentFloat = document.querySelector('.contentFloat');
+const $itemFloat = document.querySelector('.itemFloat');
+const $figures = document.querySelectorAll('.figure');
+const $figureContent = document.querySelector('.figureContent');
+const $hoverImg = document.querySelector('.hoverImg');
+const $menuOut = document.querySelector('.menuOut');
+const $figcaption = document.getElementsByTagName('figcaption');
 
-const closeBtn = document.getElementById('closeBtn');
-const mobileBtn = document.getElementById('mobileBtn');
-const mainMenu = document.getElementById('mainMenu');
+const $closeBtn = document.getElementById('closeBtn');
+const $mobileBtn = document.getElementById('mobileBtn');
+const $mainMenu = document.getElementById('mainMenu');
 
 
+    for(let i = 0; i < $figures.length;++i) {
+        const $figure = $figures[i];
+        $figure.addEventListener('click', function() {
+            $figureContent.style.display = 'block';
+            $hoverImg.style.opacity = '0.3';
+        });     
+    console.log($figure.length);    
+    };
 
 
-figure.addEventListener('click', function () {
-    figureContent.style.display = 'block';
-    hoverImg.style.opacity = '0.3';
-    figcaption.style.display = 'none';
+// figureClick.addEventListener('click', function () {
+//     figureContent.style.display = 'block';
+//     hoverImg.style.opacity = '0.3';
+//     figcaption.style.display = 'none';
 
-});
+// });
 
 
 
 
 // 모바일 메뉴버튼
+
 mobileBtn.addEventListener('click', function (e) {
     mainMenu.classList.add('on');
     mobileBtn.style.display = 'none';
@@ -37,3 +47,4 @@ closeBtn.addEventListener('click', function (e) {
     mobileBtn.style.display = 'block';
     closeBtn.style.display = 'none';
 });
+
