3635ffb081730b8ef1e6024f3c21483e35c56e55
HeoYongJe <hyj.campus@gmail.com>
2021-02-19 03:13:11 +0900

    버튼작업 및 css 작업

--- js/mainScript.js
+++ js/mainScript.js
@@ -7,19 +7,21 @@ const $hoverImg = document.querySelector('.hoverImg');
 const $boxItem = document.querySelector('.boxItem');
 const $boxText = document.querySelector('.boxText');
 
+const $mainMenu = document.getElementById('mainMenu');
+const $mainBtn = document.getElementById('mainBtn');
 const $closeBtn = document.getElementById('closeBtn');
 const $mobileBtn = document.getElementById('mobileBtn');
-const $contentBtn = document.querySelectorAll('contentBtn');
+const $contentBtn = document.querySelector('.contentBtn');
 
 
-    // for(let i = 0; i < $figures.length;++i) {
-    //     const $figure = $figures[i];
-    //     $figure.addEventListener('click', function() {
-    //         $figureContent.style.display = 'block';
-    //         $hoverImg.style.opacity = '0.3';
-    //     });     
-    // console.log($figure.length);    
-    // };
+// for(let i = 0; i < $figures.length;++i) {
+//     const $figure = $figures[i];
+//     $figure.addEventListener('click', function() {
+//         $figureContent.style.display = 'block';
+//         $hoverImg.style.opacity = '0.3';
+//     });     
+// console.log($figure.length);    
+// };
 
 
 // figureClick.addEventListener('click', function () {
@@ -29,34 +31,60 @@ const $contentBtn = document.querySelectorAll('contentBtn');
 
 // });
 
-focusMethod = function getFocus() {
-    if($boxItem.focus()){
-        boxText.style.display = 'flex';
-        // mainMenu.classList.add('on');
-    }
-  }
+// focusMethod = function getFocus() {
+//     if($boxItem.focus()){
+//         boxText.style.display = 'flex';
+//         // mainMenu.classList.add('on');
+//     }
+//   }
+
 
 
 
 // 모바일 메뉴버튼
-
-$contentBtn.addEventListener('click', function (e) {
-    mainMenu.style.display = 'none';
-    mobileBtn.style.display = 'block';
-    closeBtn.style.display = 'none';
-});
+for (let i = 0; i < $mainMenu.length; i++) {
+    $contentBtn.addEventListener('click', function () {
+        mainMenu.classList.remove('on');
+        mobileBtn.style.display = 'block';
+        closeBtn.style.display = 'none';
+    });
+}
+console.log($contentBtn)
+console.log($mainMenu)
 
 $mobileBtn.addEventListener('click', function (e) {
-    // mainMenu.classList.add('on');
-    mainMenu.style.display = 'block';
+    mainMenu.classList.add('on');
+    // mainMenu.style.display = 'block';
     mobileBtn.style.display = 'none';
     closeBtn.style.display = 'block';
 });
 
 $closeBtn.addEventListener('click', function (e) {
-
-    mainMenu.style.display = 'none';
+    mainMenu.classList.remove('on');
+    // mainMenu.style.display = 'none';
     mobileBtn.style.display = 'block';
     closeBtn.style.display = 'none';
 });
 
+
+
+
+
+
+
+// const menuOpenArea = () => ([...$mainMenu.classList].includes('on'));
+// const setOpen = (boolean) => {
+//     if (boolean) {
+//         mainMenu.classList.add('on');
+//         // mainMenu.style.display = 'block';
+//         mobileBtn.style.display = 'none';
+//         closeBtn.style.display = 'block';
+//     } else {
+//         mainMenu.classList.remove('on');
+//         // mainMenu.style.display = 'none';
+//         mobileBtn.style.display = 'block';
+//         closeBtn.style.display = 'none';
+//     }
+// };
+
+// $mobileBtn.addEventListener('click', () => setOpen(menuOpenArea()));
